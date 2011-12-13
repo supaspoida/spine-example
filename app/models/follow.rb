a56bde3041aec30eb3550894c099697b53317625
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2011-12-13 15:59:53 -0500

    Show follows on root page

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,7 +3,18 @@ class Follow
 
   attr_accessor :person
 
+  def self.people
+    all.map { |f| Person.new(name: f.name) }
+  end
+
+  def self.all
+    Persistence::Redis::Follow.all.map do |name|
+      new(name: name)
+    end
+  end
+
   def save
+    Persistence::Redis::Follow.create(person)
     valid?
   end
 
---
+++ lib/persistence/redis/follow.rb
@@ -0,0 +1,24 @@
+module Persistence
+  module Redis
+    class Follow
+      include Connection
+
+      def self.all
+        Array.wrap(redis.smembers(key))
+      end
+
+      def self.create(person)
+        redis.sadd(key, person)
+      end
+
+      def self.key
+        'follows'
+      end
+
+      def key
+        self.class.key
+      end
+
+    end
+  end
+end
