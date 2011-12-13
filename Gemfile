aa0b079d940a050d733fdd9f4b65d86945f595e6
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2011-12-13 09:29:56 -0500

    App boilerplate
      rails new ../spine-example -O -T
      Add haml-rails
      Convert layout to haml
      Add decent_exposure
      Convert application.css to sass
      Add informal
      Add pry
      Add redis

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source :rubygems
+
+gem 'rails', '3.1.3'
+
+gem 'decent_exposure'
+gem 'haml-rails'
+gem 'informal'
+gem 'jquery-rails'
+gem 'redis'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier',     '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'pry'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpineExample::Application.config.secret_token = '742a77676088be959ae9cfa21708377ffdaaf07c8d69b2d1e93a29162f0ec21878b83d8f608fa4dea8643e2f90302af5d8672dbe5d173f5e7456bfee41e15a93'
