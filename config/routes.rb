0c1e54fc2bb7152109286f57329e2486431eea26
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2011-12-13 23:02:04 -0500

    People#index API endpoint
    * This is where the Person model will point to for persistence via the
      spine app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SpineExample::Application.routes.draw do
+  namespace :api do
+    resources :people, only: :index
+  end
   resources :follows, only: [:create, :destroy]
   root to: 'demos#following'
 end
