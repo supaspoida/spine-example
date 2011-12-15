246025d00a4d9bc313ca0a3bd5be5d5ca389a8c3
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2011-12-14 21:58:01 -0500

    Move Follow model into API namespace
    * So rails helpers don't try to generate the wrong routes

---
+++ app/models/api/follow.rb
@@ -0,0 +1,47 @@
+module Api
+  class Follow
+    include Informal::Model
+
+    attr_accessor :id, :name
+
+    def self.destroy_all
+      all.map &:destroy
+    end
+
+    def self.people
+      all.map { |f| Person.new(name: f.name) }
+    end
+
+    def self.all
+      Persistence::Redis::Follow.all.map do |name|
+        new(name: name)
+      end
+    end
+
+    def as_json(*args)
+      { id: name, name: name }
+    end
+
+    def destroy
+      Persistence::Redis::Follow.destroy(name)
+    end
+
+    def name
+      @name || id
+    end
+
+    def to_param
+      name
+    end
+
+    def save
+      Persistence::Redis::Follow.create(name)
+      valid?
+    end
+
+    def persisted?
+      name.present? && Persistence::Redis::Follow.exists?(name)
+    end
+
+  end
+end
